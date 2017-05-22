c58f2efb6fb75cbe173651bed86e9cc0cccb4fd6
toby <toby.hw@live.co.uk>
2017-05-22 16:53:16 +0100

    Returns HTML img string - adds /me

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   'Oooooo! Can do!!'
@@ -6,4 +7,14 @@ end
 
 get '/secret' do
   "I'm Mr Meeseeks! Look at me!!"
+end
+
+get '/life' do
+  "I JUST WANNA DIE!"
+end
+
+get '/me' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://4.bp.blogspot.com/-NZ0AG7EvydY/VpkbubImaiI/AAAAAAAA_XM/hxAOv6McOYM/s1600/Cryptozoic%2BEntertainment%2BRick%2Band%2BMorty%2BMr%2BMeeseeks%2BBox%2BO%2BFun%2BDice%2BGame.png'><p>'HI! I'M MR MEESEEKS! LOOK AT ME!'</p>
+  </div>"
 end
